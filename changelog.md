43e3de53bb6749b81ffa95052a5b9d5d2e7112e0
Vanderhoof <ddddsa@gmail.com>
2023-01-06 17:34:08 +0100

    Update changelog, bump version

--- changelog.md
+++ changelog.md
@@ -1,3 +1,7 @@
+# 0.2.0
+
+- Fix: rename cluster to collection (#1 thanks @flozz)
+
 # 0.1.1
 
 - Add more tests, more docs and minor adjustments.
--- setup.py
+++ setup.py
@@ -13,11 +13,11 @@ except FileNotFoundError:
 
 setup(
     name='PyNuclino',
-    python_requires='>=3.5',
+    python_requires='>=3.7',
     description=SHORT_DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type='text/markdown',
-    version='0.1.1',
+    version='0.2.0',
     author='Daniel Minukhin',
     author_email='ddddsa@gmail.com',
     url='https://github.com/Vanderhoof/PyNuclino',
